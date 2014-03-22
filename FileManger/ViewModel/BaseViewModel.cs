ddb2a4df2024fa9c3413e5c8389f61f35cd22221
Alex4o <mrabonin@gmail.com>
2014-03-22 21:51:55 +0200

    First Files

---
+++ FileManger/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+	#region PropertyChanged
+	public event PropertyChangedEventHandler PropertyChanged;
+	public void NotifyPropertyChanged(string str)
+	{
+		if (PropertyChanged != null)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(str));
+		}
+	}
+	#endregion
+
+}
---
+++ FileManger/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.IO;
+namespace FileManger.ViewModel
+{
+	class MainViewModel : BaseViewModel
+	{
+		ObservableCollection<String> files;
+
+		public MainViewModel()
+		{
+			files = new ObservableCollection<string>();
+			foreach(var file in Directory.GetFiles("C://")){
+				files.Add(file);
+			}
+			
+		}
+
+		public ObservableCollection<String> Files
+		{
+			get { 
+				return files; 
+			}
+			set {
+				files = value;
+				NotifyPropertyChanged("Files");
+			}
+		}
+
+
+	}
+}
